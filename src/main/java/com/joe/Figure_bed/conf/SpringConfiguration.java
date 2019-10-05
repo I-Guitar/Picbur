d5c964379f9778ba8e2e3d81e39b0286565617dc
joe <i-guitar@outlook.com>
2019-10-05 19:38:05 +0800

    解决上传图片大小限制，添加spring配置文件

---
+++ src/main/java/com/joe/Figure_bed/conf/SpringConfiguration.java
@@ -0,0 +1,28 @@
+package com.joe.Figure_bed.conf;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * Created by joe on 2019/10/5
+ *
+ * spring 配置类
+ */
+@Configuration
+public class SpringConfiguration {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize(DataSize.ofMegabytes(100));
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.ofMegabytes(100));
+        return factory.createMultipartConfig();
+    }
+
+}
