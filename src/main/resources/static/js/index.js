432326d92a17e51c62e969098c6854acef704b99
joe <i-guitar@outlook.com>
2020-02-09 17:24:54 +0800

    修改优化

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,10 @@
 $(function () {
+    functionRegister();
 
     init();
 
     $("#commit").on("click", function () {
+        $("#masking-bg").removeAttr("hidden");
         let formData = new FormData();
         formData.append('image', $('#image')[0].files[0]);  //添加图片信息的参数
         // formData.append('sizeid', 123);  //添加其他参数
@@ -15,12 +17,35 @@ $(function () {
             contentType: false, // 告诉jQuery不要去设置Content-Type请求头
             success: function (data) {
                 uploadSuccess(data);
+                $("#masking-bg").attr("hidden", "hidden");
             },
             error: function (data) {
+                $("#masking-bg").attr("hidden", "hidden");
                 window.alert("Failed! => " + data);
             }
         });
     });
+
+    /**
+     * 点击拷贝链接
+     * */
+    $("body").on("click", ".copy", function (e) {
+        let urlDom = $(e.target).parents(".col-md-4").find("span.url");
+        let cacheInput = document.getElementById("copy-cache");
+        cacheInput.value = urlDom.text();
+        cacheInput.select();
+        document.execCommand("Copy");
+        $('.alert').html('复制成功').addClass('alert-success').show().delay(1500).fadeOut();
+    });
+
+    /**
+     * 方法注册
+     * */
+    function functionRegister() {
+        Date.prototype.format = function (fmStr) {
+            return dateFormat(fmStr, this);
+        }
+    }
 
     /**
      * 初始化
@@ -33,7 +58,20 @@ $(function () {
      * 上传成功处理
      * */
     function uploadSuccess(record) {
-        $("#last-img").attr("src", record.url);
+        let lasUploadDom = $("#last-upload");
+        // 设置图片
+        let imgDom = lasUploadDom.find("img");
+        imgDom.attr("src", record.url + "?x-oss-process=image/resize,m_pad,h_200,w_200");
+        // 设置url
+        let urlDom = lasUploadDom.find(".url");
+        urlDom.text(record.url);
+        urlDom.attr("title", record.url);
+        // 设置time
+        let timeDom = lasUploadDom.find(".time");
+        let formatTime = new Date(record.uploadTime).format("yyyy-MM-dd HH:mm:ss");
+        timeDom.text(formatTime);
+        timeDom.attr("title", formatTime);
+
         prependToRecordDiv(record);
         recordLimit();
     }
@@ -63,9 +101,19 @@ $(function () {
      * */
     function prependToRecordDiv(record) {
         let clone = $("#upload-records-div>div:nth-child(1)").clone();
-        clone.find("img").attr("src", record.url);
         clone.removeAttr("hidden");
-        clone.find("p.url").text(record.url);
+        // 设置图片
+        clone.find("img").attr("src", record.url + "?x-oss-process=image/resize,m_pad,h_200,w_200");
+        // 设置url
+        let urlDom = clone.find(".url");
+        urlDom.text(record.url);
+        urlDom.attr("title", record.url);
+        // 设置time
+        let timeDom = clone.find(".time");
+        let formatTime = new Date(record.uploadTime).format("yyyy-MM-dd HH:mm:ss");
+        timeDom.text(formatTime);
+        timeDom.attr("title", formatTime);
+
         $("#upload-records-div").prepend(clone);
     }
 
@@ -79,6 +127,29 @@ $(function () {
         for (let i = 0; i < records.length - 9; i++) {
             $("#upload-records-div>div:last-child").remove();
         }
+    }
+
+    /**
+     * 时间格式化
+     * */
+    function dateFormat(fmt, date) {
+        let ret;
+        const opt = {
+            "y+": date.getFullYear().toString(),        // 年
+            "M+": (date.getMonth() + 1).toString(),     // 月
+            "d+": date.getDate().toString(),            // 日
+            "H+": date.getHours().toString(),           // 时
+            "m+": date.getMinutes().toString(),         // 分
+            "s+": date.getSeconds().toString()          // 秒
+            // 有其他格式化字符需求可以继续添加，必须转化成字符串
+        };
+        for (let k in opt) {
+            ret = new RegExp("(" + k + ")").exec(fmt);
+            if (ret) {
+                fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+            }
+        }
+        return fmt;
     }
 
 });
